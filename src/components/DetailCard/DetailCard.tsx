45a619116a38a05a0514a2561d9f3721a7295f38
Runar <github.runar@gmail.com>
2023-08-17 23:11:49 +0300

    detail page completed

--- src/components/DetailCard/DetailCard.tsx
+++ src/components/DetailCard/DetailCard.tsx
@@ -5,6 +5,9 @@ import { extractTextInBrackets } from '@/shared/utils/extractTextInBrackets'
 import { formatDiametr } from '@/shared/utils/formatDiametr'
 import dangerous from '../../../public/assets/dangerous.svg'
 import s from './detailCard.module.scss'
+import { spawn } from 'child_process'
+import { formatDate } from '@/shared/utils/formatDate'
+import { formatDistance } from '@/shared/utils/formatDistance'
 
 interface DetailCardProps {
   oneAsteroid: AsteroidData
@@ -16,16 +19,60 @@ export const DetailCard = ({ oneAsteroid }: DetailCardProps) => {
     oneAsteroid?.estimated_diameter?.meters.estimated_diameter_min,
     oneAsteroid?.estimated_diameter?.meters.estimated_diameter_max
   )
-  console.log(oneAsteroid.close_approach_data)
+  console.log(oneAsteroid)
   return (
-    <ul>
-      <li>Name: {name}</li>
-      <li>Diameter: Ø{diameter}</li>
-      <li>
-        {oneAsteroid?.is_potentially_hazardous_asteroid && (
-          <Image src={dangerous} alt='dangerous' />
-        )}
-      </li>
-    </ul>
+    <div className={s.flex}>
+      <div className={s.detail}>
+        <ul className={s.detailList}>
+          <li className={s.item}> Имя: {name}</li>
+          <li className={s.item}>Диаметр: Ø{diameter}</li>
+          <li className={s.item}>
+            Абсолютная магнитуда: {oneAsteroid.absolute_magnitude_h}
+          </li>
+          <li>
+            {oneAsteroid?.is_potentially_hazardous_asteroid && (
+              <Image src={dangerous} alt='dangerous' />
+            )}
+          </li>
+        </ul>
+      </div>
+      <div className={s.listApproachesWrapper}>
+        <h3 className={s.title}>Список всех сближений</h3>
+        <div className={s.listApproaches}>
+          {oneAsteroid.close_approach_data?.map((item, id) => {
+            return (
+              <div key={id} className={s.itemList}>
+                <div className={s.item}>
+                  Дата приближения:&nbsp;
+                  {formatDate(item.close_approach_date)}
+                </div>
+                <div className={s.item}>
+                  Скорость KM в час:&nbsp;
+                  {item.relative_velocity.kilometers_per_hour}
+                </div>
+                <div className={s.item}>
+                  Км в секунду:&nbsp;
+                  {item.relative_velocity.kilometers_per_second}
+                </div>
+                <div className={s.item}>
+                  Миль в час:&nbsp;
+                  {item.relative_velocity.miles_per_hour}
+                </div>
+                <div className={s.item}>
+                  Вокруг чего летят:&nbsp;
+                  {item.orbiting_body}
+                </div>
+
+                <div>
+                  Расстояние до земли:&nbsp;
+                  {formatDistance(parseInt(item.miss_distance.kilometers))} km
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
   )
 }
+//relative_velocity
--- src/shared/interfaces/interfaces.ts
+++ src/shared/interfaces/interfaces.ts
@@ -39,6 +39,7 @@ export interface AsteroidData {
   name_limited: string
   nasa_jpl_url: string
   neo_reference_id: string
+  orbital_data: Orbital
 }
 
 interface ApproachData {
@@ -58,6 +59,36 @@ interface ApproachData {
     miles_per_hour: string
   }
   orbiting_body: string
+}
+
+interface Orbital {
+  aphelion_distance: string
+  ascending_node_longitude: string
+  data_arc_in_days: number
+  eccentricity: string
+  epoch_osculation: string
+  equinox: string
+  first_observation_date: string
+  inclination: string
+  jupiter_tisserand_invariant: string
+  last_observation_date: string
+  mean_anomaly: string
+  mean_motion: string
+  minimum_orbit_intersection: string
+  observations_used: number
+  orbit_class: {
+    orbit_class_type: string
+    orbit_class_range: string
+    orbit_class_description: string
+  }
+  orbit_determination_date: string
+  orbit_id: string
+  orbit_uncertainty: string
+  orbital_period: string
+  perihelion_argument: string
+  perihelion_distance: string
+  perihelion_time: string
+  semi_major_axis: string
 }
 
 interface Diameter {
