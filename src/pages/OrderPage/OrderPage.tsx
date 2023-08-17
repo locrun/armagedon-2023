c690e92e7d6fd6acf7aca8d59919bf295c3a0b54
Runar <github.runar@gmail.com>
2023-08-17 19:19:44 +0300

    fix: import

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -1,11 +1,9 @@
 import React, { useMemo } from 'react'
 import { useGlobalContext } from '@/features/Context/store'
 import s from './order.module.scss'
-import {
-  ListItem,
-  ListItemProps,
-} from '@/components/AsteroidsList/ListItem/ListItem'
+import { ListItem } from '@/components/AsteroidsList/ListItem/ListItem'
 import { formatAsteroidData } from '@/shared/utils/formatAsteroidData'
+import { AsteroidProps } from '@/shared/interfaces/interfaces'
 
 export const OrderPage = () => {
   const { cartItems } = useGlobalContext()
@@ -15,7 +13,7 @@ export const OrderPage = () => {
     <div className={s.orders}>
       <h1 className={s.title}>Заказ отправлен!</h1>
       <div>
-        {newFormattedAsteroidsData?.map((item: ListItemProps) => {
+        {newFormattedAsteroidsData?.map((item: AsteroidProps) => {
           return <ListItem key={item.id} {...item} />
         })}
       </div>
