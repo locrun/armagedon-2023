bda9228e37f34015c20bd35083e095cb0a57b11a
Runar <github.runar@gmail.com>
2023-08-18 10:44:58 +0300

    add PassionOneRegular font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { NextPage } from "next"
-import { HomePage } from "@/pages"
+import { NextPage } from 'next'
+import { HomePage } from '@/pages'
 
 const Home: NextPage = () => <HomePage />
 
